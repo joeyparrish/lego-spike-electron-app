a91a77c8f7974e7127e76608061da01bb5607dd5
Joey Parrish <joey.parrish@gmail.com>
2024-12-26 11:13:03 -0800

    Basic Spike app from electron template

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+const started = require('electron-squirrel-startup');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (started) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+/*
+    width: 800,
+    height: 600,
+*/
+  });
+  mainWindow.removeMenu();
+  mainWindow.loadURL('https://spike.legoeducation.com/prime/lobby/');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
